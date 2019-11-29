2150baeb163d9609d45514e96b5f06b546d3c345
David <contact.davidrobert@gmail.com>
2019-11-29 16:43:41 +0100

    entity type in form

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -22,7 +24,21 @@ class BookType extends AbstractType
                     'Autobio' => 'autobio'
                 ]
             ])
-            ->add('author')
+
+            // Je créé un nouveau champs de formulaire
+            // ce champs est pour la propriété 'author'
+            // vu que ce champs contient une relation vers
+            // une autre entité, le type choisi doit être
+            // EntityType
+            ->add('author', EntityType::class, [
+                // je sélectionne l'entité à afficher, ici
+                // Author car ma relation fait référence aux auteurs
+                'class' => Author::class,
+                // je choisi la propriété d'Author qui s'affiche
+                // dans le select du html
+                'choice_label' => 'lastName',
+            ])
+
             ->add('inStock')
             ->add('nbPages')
             ->add('Envoyer', SubmitType::class)
